31d2893faf7ebb1ebbdee46524cfce90066d6c43
Oris Walft <46623531+oriswalft@users.noreply.github.com>
2022-10-18 10:33:32 +0200

    first commit

---
+++ src/main/java/exo/tp2/App.java
@@ -0,0 +1,8 @@
+package exo.tp2;
+
+public class App {
+    
+    public static void main(String[] args) {
+    }
+    
+}
---
+++ src/test/java/exo/tp2/AppTest.java
@@ -0,0 +1,13 @@
+package exo.tp2;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+}
